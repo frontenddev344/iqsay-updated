39aa5791d5b54efd36ffea98d7616956904277f2
leetbadd288 <frontenddev344@gmail.com>
2025-07-30 11:22:56 +0530

    changes

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+AOS.init({
+    duration: 800, // animation duration
+    easing: 'ease-out', // animation easing
+    once: true, // whether animation should happen only once
+    offset: 100, // offset (in px) from the original trigger point
+});
